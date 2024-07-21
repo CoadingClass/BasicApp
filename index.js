a3e580a22bf6b893712eeff3b22e5b3043fb5dd6
CoadingClass <govindsdr8055@gmail.com>
2024-07-21 11:44:29 +0530

    First - Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello Kususm ");
+
+const express = require("express");
+require("dotenv").config();
+const app = express();
+// const port = 3000;
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Kusum I Love You ");
+});
+
+app.get("/kusum", (req, res) => {
+  res.send("<h1>Love you Kusum Your Govind </h1>");
+});
+
+app.listen(port, () => {
+  console.log(`⚙︎ Server is Running on =>> http://localhost:${port}`);
+});
